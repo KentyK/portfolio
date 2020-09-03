508793ec698f73fcedbdf636f2cb9ebfa0796bba
KentyK <kentokatsumata@me.com>
2020-09-03 12:45:10 +0900

    fixed splash bug in safari

--- js/work.js
+++ js/work.js
@@ -5,6 +5,8 @@ const splash = document.querySelector('.splash');
 window.addEventListener('load', (event) => {
     var referURL = document.referrer;
     var path = referURL.replace(/^https?:\/\//, '').split('/');
+    console.log(path)
+
     if (path[1] != "works") {
         setTimeout(() => {
             splash.classList.add('display-none');
